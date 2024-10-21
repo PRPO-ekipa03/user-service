e60061817fd96e62da919887cbfe848263901782
jakepel03 <jaka.pelko03@gmail.com>
2024-10-21 15:15:14 +0200

    Initial commit

---
+++ src/test/java/si/uni/prpo/group03/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package si.uni.prpo.group03.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
