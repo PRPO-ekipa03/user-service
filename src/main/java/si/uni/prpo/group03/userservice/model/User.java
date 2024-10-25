d75af353c93bde70eb474e085edc953dcd4cfd39
jakepel03 <jaka.pelko03@gmail.com>
2024-10-25 18:30:13 +0200

    Added User model

---
+++ src/main/java/si/uni/prpo/group03/userservice/model/User.java
@@ -0,0 +1,133 @@
+package si.uni.prpo.group03.userservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "Username cannot be empty")
+    private String username;
+
+    @Column(name = "first_name", nullable = false)
+    @NotBlank(message = "First name cannot be empty")
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    @NotBlank(message = "Last name cannot be empty")
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Password cannot be empty")
+    private String password;
+
+    @Column(nullable = false, updatable = false)
+    private Timestamp created;
+
+    @Column
+    private Timestamp updated;
+
+    @PrePersist
+    protected void onCreate() {
+        Instant now = Instant.now();
+        this.updated = Timestamp.from(now);
+        this.created = Timestamp.from(now);
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updated = Timestamp.from(Instant.now());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+    
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    public Timestamp getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Timestamp updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", created=" + created +
+                ", updated=" + updated +
+                '}';
+    }
+}
