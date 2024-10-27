36fe5309aeebcfc85925a96639c8787cabf86bdf
jakepel03 <jaka.pelko03@gmail.com>
2024-10-27 21:41:34 +0100

    Implemented controller with basic endpoints

---
+++ src/main/java/si/uni/prpo/group03/userservice/controller/UserController.java
@@ -0,0 +1,50 @@
+package si.uni.prpo.group03.userservice.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import si.uni.prpo.group03.userservice.dto.LoginRequestDTO;
+import si.uni.prpo.group03.userservice.dto.RegisterRequestDTO;
+import si.uni.prpo.group03.userservice.dto.UserDTO;
+import si.uni.prpo.group03.userservice.dto.UserUpdateDTO;
+import si.uni.prpo.group03.userservice.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDTO> registerUser(@Valid @RequestBody RegisterRequestDTO registerRequest) {
+        UserDTO user = userService.registerUser(registerRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserDTO> loginUser(@Valid @RequestBody LoginRequestDTO loginRequest) {
+        UserDTO user = userService.loginUser(loginRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUser(@PathVariable Long id) {
+        UserDTO user = userService.getUserById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(user);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO userUpdateRequest) {
+        UserDTO updatedUser = userService.updateUser(id, userUpdateRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+    }
+}
