abbf0913d0b5f595c61b57bf81a0e4e37ab6bb57
jakepel03 <jaka.pelko03@gmail.com>
2024-11-25 22:25:58 +0100

    Implemented JWT authentication

---
+++ src/main/java/si/uni/prpo/group03/userservice/service/JwtService.java
@@ -0,0 +1,64 @@
+package si.uni.prpo.group03.userservice.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.impl.lang.Function;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import si.uni.prpo.group03.userservice.model.User;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    public String extractSubject(String jwtToken) {
+        return extractClaim(jwtToken, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String jwtToken, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(jwtToken);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String jwtToken) {
+        return Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(jwtToken).getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public boolean validateToken(String jwtToken, User userDetails) {
+        final String userId = extractSubject(jwtToken);
+        final String userDetailsId = userDetails.getId().toString();
+        return userId.equals(userDetailsId) && !isTokenExpired(jwtToken);
+    }
+
+    private boolean isTokenExpired(String jwtToken) {
+        return extractExpiration(jwtToken).before(new Date());
+    }
+
+    private Date extractExpiration(String jwtToken) {
+        return extractClaim(jwtToken, Claims::getExpiration);
+    }
+
+    public String generateToken(Long userId) {
+        return createToken(userId.toString());
+    }
+
+    private String createToken(String userId) {
+        return Jwts.builder()
+                .subject(userId)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+}
