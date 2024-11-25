8fe4592b546ea45aa9f91739d809a83e92bba30c
jakepel03 <jaka.pelko03@gmail.com>
2024-11-25 22:28:59 +0100

    Separated User and auth controller

--- src/main/java/si/uni/prpo/group03/userservice/controller/UserController.java
+++ src/main/java/si/uni/prpo/group03/userservice/controller/UserController.java
@@ -15,36 +15,6 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/register")
-    public ResponseEntity<UserDTO> registerUser(@Valid @RequestBody RegisterRequestDTO registerRequest) {
-        UserDTO user = userService.registerUser(registerRequest);
-        return ResponseEntity.status(HttpStatus.CREATED).body(user);
-    }
-
-    @GetMapping("/confirm")
-    public ResponseEntity<String> confirmUser(@RequestParam("token") String token) {
-        userService.confirmUser(token);
-        return ResponseEntity.status(HttpStatus.OK).body("Account confirmed successfully!");
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<UserDTO> loginUser(@Valid @RequestBody LoginRequestDTO loginRequest) {
-        UserDTO user = userService.loginUser(loginRequest);
-        return ResponseEntity.status(HttpStatus.OK).body(user);
-    }
-
-    @PostMapping("/password-reset-request")
-    public ResponseEntity<String> requestPasswordReset(@Valid @RequestBody PasswordResetReqDTO passwordResetRequest) {
-        userService.requestPasswordReset(passwordResetRequest);
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Password reset email sent successfully!");
-    }
-
-    @PostMapping("/password-reset")
-    public ResponseEntity<String> resetPassword(@Valid @RequestBody PasswordResetDTO passwordReset) {
-        userService.resetPassword(passwordReset);
-        return ResponseEntity.status(HttpStatus.OK).body("Password reset successfully!");
-    }
-
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getUser(@PathVariable Long id) {
         UserDTO user = userService.getUserById(id);
